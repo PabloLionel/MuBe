1af4752554305086d85dca533cdd54e4720df8d8
MuchutGuido <guido.muchut@gmail.com>
2018-05-10 08:19:32 -0300

    Implentacion de CRUDs y correccion de errores

--- src/server/run.py
+++ src/server/run.py
@@ -10,54 +10,56 @@ def index():
 @app.route("/testChi", methods=['POST'])
 def testChi():
   try:
-    print(req.json)
+    return json.dumps(result.respuestaChicuadrado(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas'],float(req.json['error'])))
   except Exception() as e:
-    print(e)
-  return json.dumps(result.respuestaChicuadrado(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas'],float(req.json['error'])))
+    return json.dumps({'errorServer': e})
+  # else:
+  #   return json.dumps(result.respuestaChicuadrado(1000, 10, 13, [250, 253], 0.1))
+
   # {modulo: 1000,cant: 10,a: 13,semillas: [250],error: 0.1}
   # enviar del cliente:
   # req.open( "POST", '/testChi', true );req.setRequestHeader("Content-Type", "application/json; charset=UTF-8");req.send( JSON.stringify({modulo: 1000,cant: 10,a: 13,semillas: [250],error: 0.1}) );
 @app.route("/ruleta", methods=['POST'])
 def ruleta():
   try:
-    print(req.json)
+    return json.dumps(result.respuestaCasoRuleta(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas']))
   except Exception() as e:
-    print(e)
-  return json.dumps(result.respuestaCasoRuleta(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas']))
+    return json.dumps({'errorServer': e})
+
   # {modulo: 1000,cant: 10,a: 13,semillas: [250]}
 @app.route("/indice", methods=['POST'])
 def indice():
   try:
-    print(req.json)
+    return json.dumps(result.respuestaIndice(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas'],float(req.json['error'])))
   except Exception() as e:
-    print(e)
-  return json.dumps(result.respuestaIndice(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas'],float(req.json['error'])))
+    return json.dumps({'errorServer': e})
+
   # {modulo: 1000,cant: 10,a: 13,semillas: [250],error: 0.1}
 
 @app.route("/normal", methods=['POST'])
 def normal():
   try:
-    print(req.json)
+    return json.dumps(result.respuestaInvNormal(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas'],req.json['x'],req.json['px'],req.json['mu'],req.json['sigma'],req.json['stockPedido'],req.json['cantPedido']))
   except Exception() as e:
-    print(e)
-  return json.dumps(result.respuestaInvNormal(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas'],req.json['x'],req.json['px'],req.json['mu'],req.json['sigma'],req.json['stockPedido'],req.json['cantPedido']))
+    return json.dumps({'errorServer': e})
+
   # {modulo: 1000,cant: 10,a: 13,semillas: [250],x: [2, 3, 4],px: [0.30, 0.40, 0.30],mu: 150,sigma: 25,stockPedido: [{'stock': 400, 'pedido': [550, 278, 196]},{'pedido': 12, 'stock': [36, 45, 52]}],cantPedido: 25}
 
 @app.route("/inventarioParcial", methods=['POST'])
 def respuestaInvParcial():
   try:
-    print(req.json)
+    return json.dumps(result.respuestaInvParcial(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas'],req.json['x1'],req.json['px1'],req.json['x2'],req.json['px2'],req.json['dic']))
   except Exception() as e:
-    print(e)
-  return json.dumps(result.respuestaInvParcial(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas'],req.json['x1'],req.json['px1'],req.json['x2'],req.json['px2'],req.json['dic']))
+    return json.dumps({'errorServer': e})
+
   # {modulo: 1000,cant: 10,a: 13,semillas: [250],x1: [2, 3, 4],px1: [0.30, 0.40, 0.30], x2: [1, 2, 3, 4, 5, 6, 7, 8, 9], px2: [0.135, 0.271, 0.271, 0.180, 0.090, 0.036, 0.012, 0.004, 0.001], dic: [{'stock': 400, 'pedido': [550, 278, 196]}, {'pedido': 12, 'stock': [36, 45, 52]}]}
 @app.route("/inventarioPoisson", methods=['POST'])
 def respuestaInvPoisson():
   try:
-    print(req.json)
+    return json.dumps(result.respuestaInvPoisson(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas'],req.json['x1'],req.json['px1'],req.json['lambda'],req.json['stockPedido'],req.json['cantPedido']))
   except Exception() as e:
-    print(e)
-  return json.dumps(result.respuestaInvPoisson(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas'],req.json['x1'],req.json['px1'],req.json['lambda'],req.json['stockPedido'],req.json['cantPedido']))
+    return json.dumps({'errorServer': e})
+
 
 
 if __name__ == "__main__":
--- src/server/static/js/mube.js
+++ src/server/static/js/mube.js
@@ -1,93 +1,126 @@
-var req = new XMLHttpRequest();
-var solictar = (url, send, f)=>{req.open("POST", url, true);req.setRequestHeader("Content-Type", "application/json; charset=UTF-8");req.send(JSON.stringify(send)); req.onload = f};
-var success = f=>{if (req.status >= 200 && req.status <= 400) { f(req.responseText);} else{}}
-document.getElementById('enviar').addEventListener('click', ajax)
-//document.getElementById('pe').addEventListener('click', pe)
-// $(function() {
-//   $('button').click(function() {
-//     //var user = $('#txtUsername').val();
-// 		//var pass = $('#txtPassword').val();
-//     $.ajax({
-//       url: '/test',
-//       contentType: 'application/json; charset=utf-8',
-//       data: JSON.stringify({
-//         user: 'pepe',
-//         password: 'tio'
-//       }),//$('form').serialize(),
-//       type: 'POST',
-//       dataType: 'json',
-
-//       success: function(response) {
-//         console.log(response);
-//       },
-//       error: function(error) {
-//         console.log(error);
-//       }
-//     });
-//     // }), function(data) {
-//     //   $("#resultado").text(data);
-//     // };
-//   });
-// });
-// function reqListener(){
-//   console.log(this.responseText);
-// }
-// request.addEventListener('send', reqListener)
-
-function ajax(){
-  solictar('/test', {user: 'pepe', password: 'tio'}, success(data=>{
-    document.getElementById('resultado')
-    .innerHTML = (data).toString()
-  }));
+function testChi(mod, can, ban, semis, err){
+  var chi = {
+    modulo: mod, //1000,
+    cant: can, //3,
+    a: ban,//13,
+    semillas: semis,//[255, 362, 124, 123],
+    error: err//0.1
+  }
+  return consultaServer('/testChi', chi)
 }
-//   request.open('POST', 'http://192.168.0.19:5555/test', true)
-//   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');// charset=UTF-8');
-//   request.onload = function() {
-//     if (request.status >= 200 && request.status < 400) {
-//       // Success!
-//       var data = JSON.parse(request.responseText);
-//       document.getElementById('resultado').innerHTML = request.responseText//stringify(request.responseText)
-//     } else {
-//       // We reached our target server, but it returned an error
 
-//     }
-//   };
-// //var data = {'pe': 25};
-//   request.send()
+function ruleta(mod, can, ban, semis){
+  var rule = {
+    modulo: mod, //1000,
+    cant: can, //3,
+    a: ban,//13,
+    semillas: semis//[255, 362, 124, 123],
+  }
+  return consultaServer('/ruleta', rule)
+}
 
-// function pe(){
-//   $.ajax({
-//     type: 'POST',
-//     url: '/test',
-//     data: JSON.stringify({
-//       mama: "erd",
-//       jesu: "diosito"
-//     }),
-//     success: function(data){
-//       console.log(data)
-//     }
-//   })
-// //   request.open('POST', 'http://192.168.0.19:5555/test', true)
-// //   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');// charset=UTF-8');
-// //   //request.send(JSON.stringify({pepe: 263}));
-// //   request.send("vamos");
+function indice(mod, can, ban, semis, err){
+  var ind = {
+    modulo: mod, //1000,
+    cant: can, //3,
+    a: ban,//13,
+    semillas: semis,//[255, 362, 124, 123],
+    error: err//0.1
+  }
+  return consultaServer('/indice', ind)
+}
+
+function normal(mod, can, ban, semi, x, px, mu, sigma, stockPedi, cantPedi){
+  var norm = {
+    modulo: mod, //1000,
+    cant: can, //3,
+    a: ban,//13,
+    semillas: semi,//[255, 362, 124, 123],
+    x: x,
+    px: px,
+    mu: mu,
+    sigma: sigma,
+    stockPedido: stockPedi,
+    cantPedido: cantPedi
+  }
+  return consultaServer('/normal', norm)
+}
+
+function InvParcial(mod, can, ban, semi, x1, px1, x2, px2, stockPedi){
+  var inv = {
+    modulo: mod,
+    cant: can,
+    a: ban,
+    semillas: semi,
+    x1: x1,
+    px1: px2,
+    x2: x2,
+    px2: px2,
+    dic:[
+    {
+      'pedido':10,
+      'stock': [20, 35, 40]
+    },{
+      'stock':25,
+      'pedido': [19, 82, 50]
+    }
+    ]
+  }
+  return consultaServer('/inventarioParcial', inv)
+}
+// var req = new XMLHttpRequest();
+//   var solicitar = (url, send, f)=>{req.open('POST', url, true); req.setRequestHeader("Content-Type", "application/json; charset=UTF-8"); req.send(JSON.stringify(send));req.onload = f;}
+//   var success = function(f){ if (req.status >= 200 && req.status <= 400) {f(console.log(req.responseText))} else {}};
+//req.response.responseText
+// function consultaServer(ur, sen){
+//   var req = new XMLHttpRequest();
+//   var solicitar = (url, send, f)=>{req.open('POST', url, true); req.setRequestHeader("Content-Type", "application/json; charset=UTF-8"); req.send(JSON.stringify(send));req.onload = f;}
+//   var success = function(f){ if (req.status >= 200 && req.status <= 400) {f(req.response.responseText)} else {}};
+//   var resul;
+//   console.log(req);
+//   //console.log(typeof resul);
+//   return solicitar(ur, sen, success(function (sen){JSON.parse(sen)})); //JSON.parse(resul);
 // }
-// function pe(){
-//   $.ajax({
-//       url: '/test',
-//       data: {
-//         dada: "papap"
-//       },
-//       type: "POST",
-//       headers: {
-//           'Content-type': 'application/x-www-form-urlencoded'
-//       }
-//   });
-  //   success : function(response){
-  //       alert("funciona bien");
-  //   },
-  //   error: function(error){
-  //       alert("No funciona");
-  //   },
-  //   dataType: 'json'
-  // });
+
+function consultaServer(ur, dat){
+  //var req = new XMLHttpRequest();
+  var solicitar = (url, send)=>{
+    var result = {};
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: JSON.stringify(send),
+      success: function(response){
+        result = Object.assign(result, response)
+      },
+      contentType: "application/json",
+      dataType: 'json'
+    })
+    return result
+  }
+  //var success = function(f){ if (req.status >= 200 && req.status <= 400) {f(req.response.responseText)} else {}};
+  //console.log(req);
+  //console.log(typeof resul);
+  return solicitar(ur, dat)
+}
+//document.getElementById('resultado').innerHTML = JSON.parse(consultaServer('/testChi', chi))//.toString()
+
+// var mube = new MuBe()
+// console.log(mube.testChi())
+//document.getElementById('res').innerHTML = (mube.testChi()).toString()
+// function aja(fu){
+//   return mube.solicitar('POST', fu, mube.success(data=> ))
+// }
+// var solictar = (url, send, f)=>{};
+// var success = f=>{if (req.status >= 200 && req.status <= 400) { f(req.responseText);} else{}}
+
+
+
+// function ajax(){
+//   solictar('/test', {user: 'pepe', password: 'tio'}, success(data=>{
+//     document.getElementById('resultado')
+//     .innerHTML = (data).toString()
+//   }));
+// }
+// }
+// document.getElementById('enviar').addEventListener('click', ajax)
