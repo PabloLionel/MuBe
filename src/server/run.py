5515fa593f3e47a3b07b4b27fffa500aa11e9ebe
Pablo L. Benitez <yaexiste_52@hotmail.com>
2018-05-10 08:16:15 -0300

    actualizacion de las vistas Front-end

--- src/server/run.py
+++ src/server/run.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template as ren, request as req, json
-app = Flask(__name__)
 from api.resources.resource import Resultado
+from api.config.cors import crossdomain
+app = Flask(__name__)
 result = Resultado()
 
 @app.route('/')
+@crossdomain(origin='*')
 def index():
   return ren('index.html')
 
 @app.route("/testChi", methods=['POST'])
+@crossdomain(origin='*')
 def testChi():
   try:
-    print(req.json)
+    return json.dumps(result.respuestaChicuadrado(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas'],float(req.json['error'])))
   except Exception() as e:
-    print(e)
-  return json.dumps(result.respuestaChicuadrado(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas'],float(req.json['error'])))
+    return json.dumps({'errorServer': e})
   # {modulo: 1000,cant: 10,a: 13,semillas: [250],error: 0.1}
   # enviar del cliente:
   # req.open( "POST", '/testChi', true );req.setRequestHeader("Content-Type", "application/json; charset=UTF-8");req.send( JSON.stringify({modulo: 1000,cant: 10,a: 13,semillas: [250],error: 0.1}) );
 @app.route("/ruleta", methods=['POST'])
+@crossdomain(origin='*')
 def ruleta():
   try:
     print(req.json)
@@ -26,6 +29,7 @@ def ruleta():
   return json.dumps(result.respuestaCasoRuleta(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas']))
   # {modulo: 1000,cant: 10,a: 13,semillas: [250]}
 @app.route("/indice", methods=['POST'])
+@crossdomain(origin='*')
 def indice():
   try:
     print(req.json)
@@ -35,6 +39,7 @@ def indice():
   # {modulo: 1000,cant: 10,a: 13,semillas: [250],error: 0.1}
 
 @app.route("/normal", methods=['POST'])
+@crossdomain(origin='*')
 def normal():
   try:
     print(req.json)
@@ -44,6 +49,7 @@ def normal():
   # {modulo: 1000,cant: 10,a: 13,semillas: [250],x: [2, 3, 4],px: [0.30, 0.40, 0.30],mu: 150,sigma: 25,stockPedido: [{'stock': 400, 'pedido': [550, 278, 196]},{'pedido': 12, 'stock': [36, 45, 52]}],cantPedido: 25}
 
 @app.route("/inventarioParcial", methods=['POST'])
+@crossdomain(origin='*')
 def respuestaInvParcial():
   try:
     print(req.json)
@@ -52,6 +58,7 @@ def respuestaInvParcial():
   return json.dumps(result.respuestaInvParcial(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas'],req.json['x1'],req.json['px1'],req.json['x2'],req.json['px2'],req.json['dic']))
   # {modulo: 1000,cant: 10,a: 13,semillas: [250],x1: [2, 3, 4],px1: [0.30, 0.40, 0.30], x2: [1, 2, 3, 4, 5, 6, 7, 8, 9], px2: [0.135, 0.271, 0.271, 0.180, 0.090, 0.036, 0.012, 0.004, 0.001], dic: [{'stock': 400, 'pedido': [550, 278, 196]}, {'pedido': 12, 'stock': [36, 45, 52]}]}
 @app.route("/inventarioPoisson", methods=['POST'])
+@crossdomain(origin='*')
 def respuestaInvPoisson():
   try:
     print(req.json)
@@ -59,6 +66,27 @@ def respuestaInvPoisson():
     print(e)
   return json.dumps(result.respuestaInvPoisson(int(req.json['modulo']),int(req.json['cant']),int(req.json['a']),req.json['semillas'],req.json['x1'],req.json['px1'],req.json['lambda'],req.json['stockPedido'],req.json['cantPedido']))
 
+@app.route("/test", methods=['POST'])
+@crossdomain(origin='*')
+def test():
+  try:
+    return json.dumps({'test': 1})
+  except Exception() as e:
+    return json.dumps({'test': 0, 'err': e})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=4444, host='0.0.0.0')
+    app.run(debug=True, port=5555, host='0.0.0.0')
+
+
+# var req = new XMLHttpRequest()
+# undefined
+# success=f=>{
+#   if (req.status >= 200 && req.status < 400) {
+#     f(req.responseText);
+#   } else {}}
+# f=>{
+#   if (req.status >= 200 && req.status < 400) {
+#     f(req.responseText);
+#   } else {}}
+# solicitar = (url,send,f)=>{req.open( "POST", url, true );req.setRequestHeader("Content-Type", "application/json; charset=UTF-8");req.send( JSON.stringify(send) );req.onload = f; }
+# (url,send,f)=>{req.open( "POST", url, true );req.setRequestHeader("Content-Type", "application/json; charset=UTF-8");req.send( JSON.stringify(send) );req.onload = f; }
