234de91c74fc6016245090440a6f0c73f82cda6f
Pablo L. Benitez <yaexiste_52@hotmail.com>
2018-06-06 10:41:09 -0300

    Implementacion Frontent para teoria de Colas

--- src/public/dev/js/lib/forms.js
+++ src/public/dev/js/lib/forms.js
@@ -4,7 +4,7 @@ var forms = Array.prototype.slice.apply(document.getElementsByTagName('form')).m
   x => ({
     id: x.dataset.id,
     form: x,
-    imputs: Array.prototype.slice.apply(x).map(inp => inp),
+    inputs: Array.prototype.slice.apply(x).map(inp => inp),
     calcular: Array.prototype.slice.apply(x.children).filter(x => x.classList.contains('calcular'))[0],
     reset: Array.prototype.slice.apply(x.children).filter(x => x.classList.contains('reset'))[0]
   })
@@ -19,30 +19,30 @@ each(forms, el => {
 var formRanking = getForm('ranking')
 var preview = Array.prototype.slice.apply(formRanking.form.parentElement.parentElement.children[1].children[1].children).slice(1, 6)
 var nsem = document.getElementById('nsemillas')
-var optionsRanking = Array.prototype.slice.apply(formRanking.imputs[3].children)
+var optionsRanking = Array.prototype.slice.apply(formRanking.inputs[3].children)
 var semillas = []
 on(formRanking.form, 'keyup', 'entrante', e => ranking(e))
 function ranking(e) {
-  switch (formRanking.imputs.indexOf(e.target)) {
+  switch (formRanking.inputs.indexOf(e.target)) {
     case 0://Modulo
-      preview[0].innerHTML = 'Modulo: ' + formRanking.imputs[0].value + ','
+      preview[0].innerHTML = 'Modulo: ' + formRanking.inputs[0].value + ','
       break;
     case 1://Cantidad
-      preview[1].innerHTML = 'Cantidad: ' + formRanking.imputs[1].value + ','
+      preview[1].innerHTML = 'Cantidad: ' + formRanking.inputs[1].value + ','
       break;
     case 2://Bandera
-      preview[2].innerHTML = 'Bandera: ' + formRanking.imputs[2].value + ','
+      preview[2].innerHTML = 'Bandera: ' + formRanking.inputs[2].value + ','
       break;
     case 3://Error
       preview[3].innerHTML = 'Error: ' + optionsRanking.filter(op => op.selected)[0].innerHTML + ','
       break;
     case 4://Semillas
       semillas = addInputs(e.target.value, 'semilla', nsem)
-      let n = formRanking.imputs.length - 5
-      formRanking.imputs.splice(5, n)
+      let n = formRanking.inputs.length - 5
+      formRanking.inputs.splice(5, n)
       each(semillas, el => {
         el.classList.add('entrante')
-        formRanking.imputs.push(el)
+        formRanking.inputs.push(el)
       })
       preview[4].innerHTML = 'Semillas: ' + semillas.map(x => x.value)
       break;
@@ -66,9 +66,9 @@ formRanking.calcular.addEventListener('click', e => {
     } else { console.error('Error en la conexion! estado: ' + req.status) }
   }
   req.send(JSON.stringify({
-    modulo: parseInt(formRanking.imputs[0].value),
-    cant: parseInt(formRanking.imputs[1].value),
-    a: parseInt(formRanking.imputs[2].value),
+    modulo: parseInt(formRanking.inputs[0].value),
+    cant: parseInt(formRanking.inputs[1].value),
+    a: parseInt(formRanking.inputs[2].value),
     error: parseFloat(optionsRanking.filter(op => op.selected)[0].innerHTML),
     semillas: semillas.map(x => parseInt(x.value))
   })
@@ -91,7 +91,7 @@ on(formInvParcial.form, 'keyup', 'entrante', e => inventarioParcial(e))
 // on(inventarioParcial.form,'click','entrante',e=>inventarioParcial(e))
 function inventarioParcial(e) {
   // console.log(e.target)
-  switch (formInvParcial.imputs.indexOf(e.target)) {
+  switch (formInvParcial.inputs.indexOf(e.target)) {
     case 0://Modulo
 
       break;
@@ -108,14 +108,14 @@ function inventarioParcial(e) {
 
       break;
     case 5://Pedidos
-      arrPedido = addInputs(formInvParcial.imputs[5].value,'pedido',nPedidos)
+      arrPedido = addInputs(formInvParcial.inputs[5].value,'pedido',nPedidos)
       each(arrPedido, el => {el.classList.add('entrante') })
       break;
     case 6://Pedido
 
       break;
     case 7://Stocks
-      arrStock = addInputs(formInvParcial.imputs[7].value,'stock',nStock)
+      arrStock = addInputs(formInvParcial.inputs[7].value,'stock',nStock)
       each(arrStock, el => {el.classList.add('entrante') })
       break;
     // default:
@@ -133,27 +133,90 @@ formInvParcial.calcular.addEventListener('click', e => {
       var request = JSON.parse(req.response)
       newInfo.appendChild(newInvParcial(request.inventario))
       cargarGraficosInvPar(request.inventario)
-      on(document.getElementById('ranking-container'),'click','list-toggle', e=>{
+      on(document.getElementById('experimentos'),'click','list-toggle', e=>{
         e.target.classList.toggle('active')
       })
     } else { console.error('Error en la conexion! Estado: ' + req.status) }
   }
 
 req.send(JSON.stringify({
-  modulo: parseInt(formInvParcial.imputs[0].value),
-  cant: parseInt(formInvParcial.imputs[1].value),
-  a: parseInt(formInvParcial.imputs[2].value),
-  semillas: [parseInt(formInvParcial.imputs[3].value)],
+  modulo: parseInt(formInvParcial.inputs[0].value),
+  cant: parseInt(formInvParcial.inputs[1].value),
+  a: parseInt(formInvParcial.inputs[2].value),
+  semillas: [parseInt(formInvParcial.inputs[3].value)],
   x1: [2, 3, 4],
   px1: [0.30, 0.40, 0.30],
   x2: [1, 2, 3, 4, 5, 6, 7, 8, 9],
   px2: [0.135, 0.271, 0.271, 0.180, 0.090, 0.036, 0.012, 0.004, 0.001],
   dic: [
-    { 'stock': parseInt(formInvParcial.imputs[4].value), 'pedido': arrPedido.map(x=>parseInt(x.value)) },
-    { 'pedido': parseInt(formInvParcial.imputs[6].value), 'stock': arrStock.map(x=>parseInt(x.value)) }
+    { 'stock': parseInt(formInvParcial.inputs[4].value), 'pedido': arrPedido.map(x=>parseInt(x.value)) },
+    { 'pedido': parseInt(formInvParcial.inputs[6].value), 'stock': arrStock.map(x=>parseInt(x.value)) }
   ]
   })
   )
   window.scrollTo(0, document.getElementById('informe').getBoundingClientRect().y)
 })
+//[[[[[[[[[[[[[[[[[[[[[[[[Formulario para Teoria de Colas]]]]]]]]]]]]]]]]]]]]]]]]
+var formTeoriaDeColas = getForm('teoriaDeColas')
+
+// var optionsTColas = Array.prototype.slice.apply(formTeoriaDeColas.inputs[4].children)
+// on(formTeoriaDeColas.form, 'keyup', 'entrante', e => teoriaDeColas(e))
+// function teoriaDeColas(e) {
+//   // console.log(e.target)
+//   switch (formTeoriaDeColas.inputs.indexOf(e.target)) {
+//     case 0://Modulo
+
+//       break;
+//     case 1://Cantidad
+
+//       break;
+//     case 2://Bandera
+
+//       break;
+//     case 3://Semilla
+
+//       break;
+//     case 4://Stock
+
+//       break;
+//     case 5://Pedidos
+//       break;
+//     case 6://Pedido
+
+//       break;
+//     case 7://Stocks
+//       break;
+//     // default:
+//     // console.log(e.target)
+//     // preview[4].innerHTML = 'Semillas: ' + semillas.map(x=>x.value)
+//   }
+// }
+formTeoriaDeColas.calcular.addEventListener('click', e => {
+  e.preventDefault()
+  removeChilds(newInfo)
+  req.open('POST', '/tColas', true);
+  req.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+  req.onload = () => {
+    if (req.status >= 200 && req.status < 400) {
+      var request = JSON.parse(req.response)
+      newInfo.appendChild(newTColas(request.tColas))
+      cargarGraficosTColas(request.tColas)
+    } else { console.error('Error en la conexion! Estado: ' + req.status) }
+  }
+  req.send(JSON.stringify({
+    modulo: parseInt(formTeoriaDeColas.inputs[0].value),
+    cant: parseInt(formTeoriaDeColas.inputs[1].value),
+    a: parseInt(formTeoriaDeColas.inputs[2].value),
+    semillas: [parseInt(formTeoriaDeColas.inputs[3].value)],
+    exp: parent(Experimentos.inputs[5]),
+    inc: parseFloat(formTeoriaDeColas.inputs[6].value),
+    corridas: parseInt(formTeoriaDeColas.inputs[7].value),
+    opcML: formTeoriaDeColas.inputs[8].checked,
+    iniM: parseInt(formTeoriaDeColas.inputs[10].value),
+    iniL: parseInt(formTeoriaDeColas.inputs[11].value)
+    })
+    )
+    window.scrollTo(0, document.getElementById('informe').getBoundingClientRect().y)
+})
+
 
--- src/public/dev/js/lib/tabs.js
+++ src/public/dev/js/lib/tabs.js
@@ -1,17 +1,61 @@
-// let btnTab = Array.prototype.slice.apply(document.querySelectorAll('.tab__head'))
-// let arrBtnTab = btnTab.map(x=>Array.prototype.slice.apply(x.childNodes)
-//                    .filter(x=>x.nodeName!='#text'))
-// let arrTabItems = Array.prototype.slice.apply(document.querySelectorAll('.tab'))
-// .map(x=>Array.prototype.slice.apply(x.childNodes)
-//                    .filter(x=>(x.className!='tab__head' && x.nodeName!='#text')))
-// arrBtnTab.forEach(x=>x[0].classList.add('active'))
-// arrTabItems.forEach(x=>x[0].classList.add('active'))
-// btnTab.forEach(el=>el.addEventListener('click',e => {
-//   let btn = e.target
-//   let i =btnTab.indexOf(btn.parentElement)
-//   let btns= arrBtnTab[i]
-//   btns.forEach(elm=>elm.classList.remove('active'))
-//   btn.classList.toggle('active')
-//   arrTabItems[i].forEach(item=>item.classList.remove('active'))
-//   arrTabItems[i][btns.indexOf(btn)].classList.toggle('active')
-// }))
+function tabs(){
+  let btnTab = Array.prototype.slice.apply(document.querySelectorAll('.tab__head'))
+  let arrBtnTab = btnTab.map(x=>Array.prototype.slice.apply(x.childNodes)
+                    .filter(x=>x.nodeName!='#text'))
+  let arrTabItems = Array.prototype.slice.apply(document.querySelectorAll('.tab'))
+  .map(x=>Array.prototype.slice.apply(x.childNodes)
+                    .filter(x=>(x.className!='tab__head' && x.nodeName!='#text')))
+  arrBtnTab.forEach(x=>x[0].classList.add('active'))
+  arrTabItems.forEach(x=>x[0].classList.add('active'))
+  btnTab.forEach(el=>el.addEventListener('click',e => {
+    let btn = e.target
+    let i =btnTab.indexOf(btn.parentElement)
+    let btns= arrBtnTab[i]
+    btns.forEach(elm=>elm.classList.remove('active'))
+    btn.classList.toggle('active')
+    arrTabItems[i].forEach(item=>item.classList.remove('active'))
+    arrTabItems[i][btns.indexOf(btn)].classList.toggle('active')
+}))
+}
+let btns_head_tab = n => {
+  let btns = []
+  for(let i = 0; i < n; ++i)
+    btns.push({
+      el: 'button',
+      text: 'Exp. 1'
+    })
+    return btns
+  }//botones para cada item
+let items = data =>{
+  let pages = []
+  let n = data.lenght
+  for(let i = 0; i < n; ++i)
+    pages.push({
+      el: 'div'
+    })
+}//items o paginas del tab
+let newTabsColas = data => newComponentHTML({
+  el: 'div',
+  attrs: [{name: 'class', val: 'tab'}],
+  child: [{
+    el: 'div',
+    attrs: [{name: 'class', val: 'tab__head'}],
+    child: btns_head_tab(data.lenght)
+  }].concat(items(data))
+})
+tabs()
+
+// .tab.w-92.m-auto
+//   .tab__head
+//     button Caso 1
+//     button Caso 2
+//     button Caso 3
+//   .tab__item
+//     h3 Caso 1
+//     p Cualquier yerba de contenido...
+//   .tab__item
+//     h3 Caso 2
+//     p Cualquier yerba de contenido...
+//   .tab__item
+//     h3 Caso 3
+//     p Cualquier yerba de contenido...
