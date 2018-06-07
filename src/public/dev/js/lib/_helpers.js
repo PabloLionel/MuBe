9edf44db268dc6c5bdd00f5a48c55dad4ef02f4e
Pablo L. Benitez <yaexiste_52@hotmail.com>
2018-06-07 16:51:28 -0300

    nueva tarea de produccion para  gulp

--- src/public/dev/js/lib/_helpers.js
+++ src/public/dev/js/lib/_helpers.js
@@ -1,7 +1,4 @@
 // var resultadoRancking = JSON.parse('{"chicuadrado": [{"aceptacion": true, "frecuencia": [1, 8, 4, 8, 4, 5, 3, 9, 3, 9], "semilla": 251, "serie": [263, 419, 447, 811, 543, 59, 767, 971, 623, 99, 287, 731, 503, 539, 7, 91, 183, 379, 927, 51], "serie0y1": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "valorChi": 11, "valornpi": 5}, {"aceptacion": true, "frecuencia": [2, 10, 3, 8, 2, 3, 4, 12, 7, 8], "semilla": 137, "serie": [781, 153, 989, 857, 141, 833, 829, 777, 101, 313, 69, 897, 661, 593, 709, 217, 821, 673, 749, 737], "serie0y1": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "valorChi": 18, "valornpi": 5}]}')
-let tColas = {'tColas': {'exp1': {'corrida': [{'numCliente': [1, 2, 3], 'tLlegada': [0, 1.8442, 1.8783], 'tServicio': [1.7132, 7.3767, 0.1363], 'tEntLlega': [0, 1.8442, 0.0341], 'tEnCola': [0, 0, 7.3426], 'tPerman': [1.7132, 7.3767, 7.4789], 'tSalida': [1.7132, 9.2209, 9.3572], 'clientsCola': [0, 0, 1], 'tOcioServer': [0, 0, 0], 'numCorrida': 1, 'mediaTeL': 0.6261, 'mediaTE': 2.4475, 'mediaTW': 5.5229, 'mediaTC': 0.3333, 'mediaTO': 0.0}, {'numCliente': [1, 2, 3], 'tLlegada': [0, 0.5379, 1.5252], 'tServicio': [1.7132, 2.1516, 3.9494], 'tEntLlega': [0, 0.5379, 0.9873], 'tEnCola': [0, 1.1753, 2.3396], 'tPerman': [1.7132, 3.3269, 6.289], 'tSalida': [1.7132, 3.8648, 7.8142], 'clientsCola': [0, 1, 2], 'tOcioServer': [0, 0, 0], 'numCorrida': 2, 'mediaTeL': 0.5084, 'mediaTE': 1.1716, 'mediaTW': 3.7764, 'mediaTC': 1.0, 'mediaTO': 0.0}, {'numCliente': [1, 2, 3], 'tLlegada': [0, 1.9815, 2.4939], 'tServicio': [1.7132, 7.9262, 2.0496], 'tEntLlega': [0, 1.9815, 0.5124], 'tEnCola': [0, 0, 7.4138], 'tPerman': [1.7132, 7.9262, 9.4634], 'tSalida': [1.7132, 9.9077, 11.9573], 'clientsCola': [0, 0, 1], 'tOcioServer': [0, 0, 0], 'numCorrida': 3, 'mediaTeL': 0.8313, 'mediaTE': 2.4713, 'mediaTW': 6.3676, 'mediaTC': 0.3333, 'mediaTO': 0.0}], 'L': 0.2, 'M': 0.8, 'mediaExpTE': 2.0301, 'mediaExpTW': 5.2223, 'mediaExpTO': 0.0}, 'exp2': {'corrida': [{'numCliente': [1, 2, 3], 'tLlegada': [0, 2.4589, 2.5043], 'tServicio': [1.7132, 7.3767, 0.1363], 'tEntLlega': [0, 2.4589, 0.0454], 'tEnCola': [0, 0, 7.3313], 'tPerman': [1.7132, 7.3767, 7.4676], 'tSalida': [1.7132, 9.8356, 9.9719], 'clientsCola': [0, 0, 1], 'tOcioServer': [0, 0, 0], 'numCorrida': 1, 'mediaTeL': 0.8348, 'mediaTE': 2.4438, 'mediaTW': 5.5192, 'mediaTC': 0.3333, 'mediaTO': 0.0}, {'numCliente': [1, 2, 3], 'tLlegada': [0, 0.7172, 2.0337], 'tServicio': [1.7132, 2.1516, 3.9494], 'tEntLlega': [0, 0.7172, 1.3165], 'tEnCola': [0, 0.996, 1.8311], 'tPerman': [1.7132, 3.1476, 5.7805], 'tSalida': [1.7132, 3.8648, 7.8142], 'clientsCola': [0, 1, 1], 'tOcioServer': [0, 0, 0], 'numCorrida': 2, 'mediaTeL': 0.6779, 'mediaTE': 0.9424, 'mediaTW': 3.5471, 'mediaTC': 0.6667, 'mediaTO': 0.0}, {'numCliente': [1, 2, 3], 'tLlegada': [0, 2.6421, 3.3253], 'tServicio': [1.7132, 7.9262, 2.0496], 'tEntLlega': [0, 2.6421, 0.6832], 'tEnCola': [0, 0, 7.243], 'tPerman': [1.7132, 7.9262, 9.2926], 'tSalida': [1.7132, 10.5683, 12.6179], 'clientsCola': [0, 0, 1], 'tOcioServer': [0, 0, 0], 'numCorrida': 3, 'mediaTeL': 1.1084, 'mediaTE': 2.4143, 'mediaTW': 6.3107, 'mediaTC': 0.3333, 'mediaTO': 0.0}], 'L':0.2, 'M': 0.6, 'mediaExpTE': 1.9335, 'mediaExpTW': 5.1257, 'mediaExpTO': 0.0}}}
-// tColas = JSON.parse(tColas)
-console.log(tColas)
 //Sleep_sort
 // Array.prototype.timeoutSort = function (f) {
 // 	this.forEach(function (n) {
@@ -110,33 +107,6 @@ function addInputs(num,label,inputs){
     state.push(newInput)
   }
   return state
-}
-let modal = (head, body, foot) => {
-  if (typeof head == 'string')
-    return newComponent({
-      el: 'div',
-      attrs: [{name: 'class', val: 'modal-content'}],
-      child:[{
-        el: 'div',
-        attrs: [{name: 'class', val: 'modal'}],
-        child: [{
-          el: 'div',
-          attrs: [{name: 'class', val: 'modal-head'}],
-          child: [{el: 'h1',text: head}]
-        },{
-          el: 'div',
-          attrs: [{name: 'class', val: 'modal-body'}],
-          child: [body]
-        },{
-          el: 'div',
-          attrs: [{name: 'class', val: 'modal-foot'}],
-          child: foot
-        }]
-      }]
-
-    })
-  else
-    throw new Error('El nombre del modal debe ser una cadena')
 }
 // function newInputExp(inputs, title, num){
 //   //  retornamos el estado de los nuevos inputs:
---
+++ src/public/dev/js/lib/modal.js
@@ -0,0 +1,28 @@
+
+// const modal = (head, body, foot) => {
+//   if (typeof head == 'string')
+//     return newComponentHTLM({
+//       el: 'div',
+//       attrs: [{name: 'class', val: 'modal-content'}],
+//       child:[{
+//         el: 'div',
+//         attrs: [{name: 'class', val: 'modal'}],
+//         child: [{
+//           el: 'div',
+//           attrs: [{name: 'class', val: 'modal-head'}],
+//           child: [{el: 'h1',text: head}]
+//         },{
+//           el: 'div',
+//           attrs: [{name: 'class', val: 'modal-body'}],
+//           child: [body]
+//         },{
+//           el: 'div',
+//           attrs: [{name: 'class', val: 'modal-foot'}],
+//           child: foot
+//         }]
+//       }]
+
+//     })
+//   else
+//     throw new Error('El nombre del modal debe ser una cadena')
+// }
